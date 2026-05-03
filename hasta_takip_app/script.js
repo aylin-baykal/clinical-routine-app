9007b5539b86102696765185a92211cc615348cb
Aylin Baykal <aylin_baykal@outlook.com>
2026-05-04 01:15:38 +0300

    script.js

--- hasta_takip_app/script.js
+++ hasta_takip_app/script.js
@@ -1,3 +1,4 @@
+alert("çalıştı");
 const days = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
 const hours = ["09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"];
 
